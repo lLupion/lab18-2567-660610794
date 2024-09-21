26ed26e46f095220d8f935d4aac830280afc8f53
Wichuda <Vichudarodna.0569@gmail.com>
2024-09-21 18:32:29 +0700

    done

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   return NextResponse.json({
     ok: true,
-    fullName: "Dome Potikanond",
-    studentId: "660610999",
+    fullName: "Wichuda Rotna",
+    studentId: "660610794",
   });
 };
